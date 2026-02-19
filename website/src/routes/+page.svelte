ab9febbe99070fcee834a274ee024d201c287cfc
robo-monk <rrobomonk@gmail.com>
2026-02-19 15:53:53 +0100

    fix typo

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -197,7 +197,7 @@
 					GitHub
 				</a>
 				<a
-					href="https://www.npmjs.com/package/@cnvx/autoform-svelte"
+					href="https://www.npmjs.com/package/autoform-svelte"
 					target="_blank"
 					rel="noopener noreferrer"
 					class="mt-2 inline-flex items-center justify-center rounded-md text-sm font-medium h-10 px-4 py-2 border border-input bg-background hover:bg-accent hover:text-accent-foreground transition-colors"
